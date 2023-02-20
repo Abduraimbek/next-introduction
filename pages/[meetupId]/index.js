37f66012f94e5006f7fb8bf498c063b27601bf0d
Abduraimbek <yarkinovabduraimbek@gmail.com>
2023-02-20 20:54:40 +0300

    some changes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,29 @@
 import MeetupDetails from '../../components/meetups/MeetUpDetails';
-import { getMeetups } from '../../utils/meetups';
 
 export default function MeetupDetailsPage(props) {
   return <MeetupDetails meetupData={props.meetupData} />;
+}
+
+async function getMeetups() {
+  const response = await fetch(
+    'https://react-http-6fe49-default-rtdb.firebaseio.com/nextjs-meetups.json'
+  );
+
+  const data = await response.json();
+
+  const meetups = [];
+
+  for (const key in data) {
+    meetups.push({
+      id: key,
+      title: data[key].title,
+      image: data[key].image,
+      description: data[key].description,
+      address: data[key].address,
+    });
+  }
+
+  return meetups;
 }
 
 export async function getStaticPaths() {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import MeetupList from '../components/meetups/MeetupList';
-import { getMeetups } from '../utils/meetups';
 
 export default function HomePage(props) {
   return (
@@ -18,18 +17,39 @@ export default function HomePage(props) {
 }
 
 // export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
+//   const meetups = await getMeetups();
 
 //   return {
 //     props: {
-//       meetups: DUMMY_MEETUPS,
+//       meetups: meetups,
 //     },
 //   };
 // }
 
 export async function getStaticProps() {
-  const meetups = await getMeetups();
+  const response = await fetch(
+    'https://react-http-6fe49-default-rtdb.firebaseio.com/nextjs-meetups.json',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  const meetups = [];
+
+  for (const key in data) {
+    meetups.push({
+      id: key,
+      title: data[key].title,
+      image: data[key].image,
+      description: data[key].description,
+      address: data[key].address,
+    });
+  }
 
   return {
     props: {
